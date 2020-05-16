85b6818c0874c6bbb15e04f57854d6017f8f2a28
Quinn Palmer <za19157@bristol.ac.uk>
2020-05-16 13:33:39 +0100

    made changes to base url in pyodide in web folder

--- web/pyodide.js
+++ web/pyodide.js
@@ -6,7 +6,8 @@ var languagePluginLoader = new Promise((resolve, reject) => {
   // This is filled in by the Makefile to be either a local file or the
   // deployed location. TODO: This should be done in a less hacky
   // way.
-  var baseURL = self.languagePluginUrl || 'https://de-luxham.github.io/web/pyodide';
+  //var baseURL = self.languagePluginUrl || 'https://de-luxham.github.io/web/pyodide';
+  var baseURL = 'https://de-luxham.github.io/';
   baseURL = baseURL.substr(0, baseURL.lastIndexOf('/')) + '/';
 
   ////////////////////////////////////////////////////////////
