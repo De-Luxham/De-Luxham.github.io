53157afb327b3b0c800ec1f2472dea9cee84f6b3
Quinn Palmer <za19157@bristol.ac.uk>
2020-06-05 17:26:52 +0100

    added language plugin loader url in graph test copy. set pyodide.js language plugin loader line back to its origonal state

--- web/pyodide.js
+++ web/pyodide.js
@@ -6,8 +6,8 @@ var languagePluginLoader = new Promise((resolve, reject) => {
   // This is filled in by the Makefile to be either a local file or the
   // deployed location. TODO: This should be done in a less hacky
   // way.
-  //var baseURL = self.languagePluginUrl || 'https://de-luxham.github.io/web/';
-  var baseURL = 'https://de-luxham.github.io/web/';
+  var baseURL = self.languagePluginUrl || 'https://de-luxham.github.io/web/';
+  //var baseURL = 'https://de-luxham.github.io/web/';
   baseURL = baseURL.substr(0, baseURL.lastIndexOf('/')) + '/';
 
   ////////////////////////////////////////////////////////////
