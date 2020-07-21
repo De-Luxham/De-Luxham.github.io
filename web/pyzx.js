17cb7a0b4a5b9b61fda52c1a2bcb998db1cdc0fe
Quinn Palmer <za19157@bristol.ac.uk>
2020-07-21 19:05:03 +0100

    PYZX NOW WORKS (we think) tested rule update and it seems to update the pyzx graph object correctly

--- web/pyzx.js
+++ web/pyzx.js
@@ -1,64 +1,31 @@
 
 
 define(['d3'], function(d3) {
-    
-    require(['pyodid'], function(pyodid) {
-    languagePluginLoader.then(function ()  {
-    console.log(pyodide.runPython('import sys'));
-    console.log(pyodide.runPython(`print('hello world')`));
-    });  
-});
-require(['pyodid'], function(pyodid) {
-    languagePluginLoader.then(function ()  {
-    pyodide.remotePath
-    pyodide.remotePath = "https://de-luxham.github.io/web/";
-    pyodide.runPython('import xyz\n');
-    });  
-})
-require(['pyodid'], function(pyodid) {
-    languagePluginLoader.then(function ()  {
-    console.log(self.language)
-    pyodide.remotePath
-    pyodide.remotePath = ["/"];
-    pyodide.runPython('import test_print\n');
-    pyodide.runPython('test_print.printer()');
-    });  
-}); 
-require(['pyodid'], function(pyodid) {
-    languagePluginLoader.then(function ()  {
-    
-    pyodide.remotePath = ["/"];
-    pyodide.runPython('import test_print\n');
-    pyodide.runPython('test_print.printer()');
-    });  
-}); 
-require(['pyodid'], function(pyodid) {
-    languagePluginLoader.then(function ()  {
-    pyodide.remotePath
-    pyodide.remotePath = "https://de-luxham.github.io/web/";
-    pyodide.runPython('import test_print\n');
-    pyodide.runPython('test_print.printer()');
-    });  
-})
-require(['pyodid'], function(pyodid) {
-    languagePluginLoader.then(function ()  {
-    pyodide.remotePath
-    pyodide.remotePath = "https://de-luxham.github.io/"
-    pyodide.runPython('import py_modules\n');
-    console.log('Modules imported');
-    });  
-});
 
+    //require(['pyodid'], function(pyodid) {
+        languagePluginLoader.then(function ()  {
+        pyodide.runPython('print("python is a go!")');
+        });  
+    //})
 
+    pythonCode = `
+      def do_work(*args):
+          import pyzx as zx
+          print("pyzx imported")
+          graph = zx.generate.cliffords(3, 3)
+          print(graph)
+          print("HELLO WORLD")
+      
+      
 
-    
-    
-  
+      import micropip
+      micropip.install('pyzx').then(do_work)`
+      languagePluginLoader.then(() => {
+        return pyodide.loadPackage(['micropip'])
+      }).then(() => {
+        pyodide.runPython(pythonCode);
+      })
 
-   
-          
-    
-    
     // styling functions
     function nodeColor(t) {
         if (t == 0) return "black";
@@ -522,19 +489,108 @@ require(['pyodid'], function(pyodid) {
             
             console.log(rule_string);
             
-             
+            pythonCode2 = `
+            import sys
+            import numpy as np
+            import random, math, os
+            import pyzx as zx
+            from fractions import Fraction
+            import webbrowser
+            import time
+            import csv
             
             
-                languagePluginLoader.then(function ()  {
-                console.log(pyodide.runPython('import sys'));
-                console.log(pyodide.runPython(`print('hello world')`));
-                pyodide.remotePath
-                pyodide.remotePath = "https://de-luxham.github.io/"
-                pyodide.runPythonAsync('import Implementer\n')
-                console.log('Implementer imported')
+            
+            #this takes a list as arguement, shape depends on the rule being implemented, but the first element 
+            #of each row should be the rule to be implemented.
+            
+            def Implement_Rules(data):
+            
+                #temporarily hardcoding graph object in
+                qubit_amount = 3
+                depth = 4
+                random.seed(1337)
+                graph = zx.generate.cliffords(qubit_amount, depth)
+            
+                for i in range(len(data)):
+                        print(type(data[i][0]),data[i][0])
+                        if data[i][0]=="s":                     #If rule to perform is spider
+                            zx.rules.apply_rule(graph, zx.rules.spider, [[int(data[i][1]),int(data[i][2])]], check_isolated_vertices=True)
+                            #display(zx.draw(graph,labels=True))
+                            print(graph)
+                            
+                        if data[i][0]=="u":  
+                            neighbours=[]                   #If rule to perform is unspider
+                            data[i][1]=int(data[i][1])          #target node
+                            data[i][2]=Fraction(data[i][2])          #targets phase 
+                            data[i][3]=int(data[i][3])          # Qubit/row index - where to position on graph
+                            data[i][4]=int(data[i][4])          #Column index (row in pyzx) where to position on graph
+                            
+                            for j in range(5, len(data[i])):       #Neeed to loop over every nearest neighbour to cast it to integer
+                                                                    #start at the nearest neighbour entries
+                                neighbours.append(int(data[i][j])) 
+                            zx.rules.unspider(graph, [data[i][1], tuple(neighbours),data[i][2] ], data[i][4] , data[i][3] )
+                            #display(zx.draw(graph,labels=True))
+                            print(graph)
+            
+            
+                #The rules called here were written by me, and require lots more testing and rewriting
+                #=============================================================================
+                        if data[i][0]=="i":                     
+                            neighbours=[]
+                            neighbours.append(int(data[i][1]))
+                            neighbours.append(int(data[i][2]))  
+                            zx.rules.add_identity(graph,tuple(neighbours),data[i][3])
+                            #display(zx.draw(graph,labels=True))
+                            print(graph)
+            
+            
+                        if data[i][0]=="ri":
+                            neighbours=[]
+                            neighbours.append(int(data[i][2]))
+                            neighbours.append(int(data[i][3]))
+                            zx.rules.remove_identity(graph,int(data[i][1]),tuple(neighbours))
+                            #display(zx.draw(graph,labels=True))
+                            print(graph)
+            
+            
+                
+            
+                #G_OUT=zx.draw(graph,labels=True)
+                #G_OUT.savefig("data/graph.png", format="png")
+                html2 ="""<h2>New pyzx graph</h2>
+                
+                <img src="./data/graph.png" alt="New Circuit" width="500" height="333">
+                </body>
+                </html>
+                """
+                
+                #p = open(r"circuits.html", "w")
+                #p.write(html2)
+                #p.close()
+                #url = os.getcwd()+'/' + 'circuits.html'
+                #webbrowser.open(url,2)
+                #==================
+                print(graph)
+            Implement_Rules(`.concat(rule_string,')')
+            
+            
+                // languagePluginLoader.then(function ()  {
+                // console.log(pyodide.runPython('import sys'));
+                // console.log(pyodide.runPython(`print('hello world')`));
+                // pyodide.remotePath
+                // pyodide.remotePath = "https://de-luxham.github.io/"
+                // pyodide.runPythonAsync('import Implementer\n')
+                // console.log('Implementer imported')
                 //pyodide.runPython('Implement_Rules('.concat(rule_string,')'))
 
-                });  
+                // }); 
+                pyodide.loadPackage('matplotlib').then(() => {
+                    pyodide.runPython(pythonCode2);
+                  });
+                // languagePluginLoader.then(() => {
+                //     pyodide.runPython(pythonCode2);
+                //   })
         
             
         
@@ -687,11 +743,11 @@ require(['pyodid'], function(pyodid) {
         //case if this is the first rule applied 
         if (rule_string.length == 2){
             
-            rule_string = "[[".concat("s",",",selected_node.name,",",dead_node.name,"]]");
+            rule_string = "[[".concat("\"s\"",",",selected_node.name,",",dead_node.name,"]]");
             
         } else {
             rule_string = rule_string.slice(0,-1);
-            fuse_string = ",[".concat("s",",",selected_node.name,",",dead_node.name,"]]");
+            fuse_string = ",[".concat("\"s\"",",",selected_node.name,",",dead_node.name,"]]");
             rule_string = rule_string.concat(fuse_string);
         }
             
